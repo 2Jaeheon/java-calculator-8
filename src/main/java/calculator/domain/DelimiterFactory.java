bc9f2d212737201251fac9edecb43d230b52c03c
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-10-16 00:18:59 +0900

    feat: 구분자 팩토리로부터 구분자 만들기
    Signed-off-by: 2jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/main/java/calculator/domain/DelimiterFactory.java
+++ src/main/java/calculator/domain/DelimiterFactory.java
@@ -1,5 +1,31 @@
 package calculator.domain;
 
+import calculator.domain.delimiter.BasicDelimiter;
+import calculator.domain.delimiter.ComplexDelimiter;
+import calculator.domain.delimiter.CustomDelimiter;
+import calculator.domain.delimiter.Delimiter;
+import java.util.Arrays;
+import java.util.List;
+
 public class DelimiterFactory {
 
+    private final List<Delimiter> delimiters;
+
+    public DelimiterFactory() {
+        this.delimiters = Arrays.asList(
+            new ComplexDelimiter(),
+            new CustomDelimiter(),
+            new BasicDelimiter()
+        );
+    }
+
+    public Delimiter findDelimiterFor(String expression) {
+        for (Delimiter delimiter : delimiters) {
+            if (delimiter.support(expression)) {
+                return delimiter;
+            }
+        }
+
+        throw new IllegalStateException("적합한 구분자를 찾을 수 없습니다.");
+    }
 }
---
+++ src/test/java/calculator/domain/DelimiterFactoryTest.java
@@ -0,0 +1,61 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import calculator.domain.delimiter.BasicDelimiter;
+import calculator.domain.delimiter.ComplexDelimiter;
+import calculator.domain.delimiter.CustomDelimiter;
+import calculator.domain.delimiter.Delimiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DelimiterFactoryTest {
+
+    private DelimiterFactory delimiterFactory;
+
+    @BeforeEach
+    void setUp() {
+        delimiterFactory = new DelimiterFactory();
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자와 기본 구분자가 동시에 사용된 경우 ComplexDelimiter를 반환해야 한다")
+    void findDelimiterForReturnComplexDelimiterForMixedCase() {
+        // given
+        String expression = "//;\n1:2,3;4";
+
+        // when
+        Delimiter delimiter = delimiterFactory.findDelimiterFor(expression);
+
+        // then
+        assertThat(delimiter).isInstanceOf(ComplexDelimiter.class);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자만 사용된 경우 CustomDelimiter를 반환해야 한다")
+    void findDelimiterForReturnCustomDelimiterForCustomCase() {
+        // given
+        String expression = "//;\n1;2;3";
+
+        // when
+        Delimiter delimiter = delimiterFactory.findDelimiterFor(expression);
+
+        // then
+        assertThat(delimiter).isInstanceOf(CustomDelimiter.class);
+    }
+
+    @Test
+    @DisplayName("기본 구분자만 사용된 경우 BasicDelimiter를 반환해야 한다")
+    void findDelimiterForReturnBasicDelimiterForBasicCase() {
+        // given
+        String expression = "1,2:3";
+
+        // when
+        Delimiter delimiter = delimiterFactory.findDelimiterFor(expression);
+
+        // then
+        assertThat(delimiter).isInstanceOf(BasicDelimiter.class);
+    }
+}
