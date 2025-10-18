344a25a0ebd6d04e8e74e54005036047f2cad5ae
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-10-18 18:37:12 +0900

    feat(Application): 애플리케이션 실행을 위한 컨트롤러 구축
    Application에서 객체를 생성하고 Controller에서 실제로 함수들을 호출해서 값을 계산
    
    세부 사항:
    - Application객체에서 객체들을 모두 생성하고 run() 메서드 하나로 엔트리 포인트 설정
    - Controller 객체에서 run() 메서드를 실행해서 입출력 및 계산 로직을 수행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import calculator.application.CalculateController;
+import calculator.domain.Adder;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterFactory;
+import calculator.domain.Splitter;
+import calculator.domain.delimiter.Delimiter;
+import calculator.view.Keyboard;
+import calculator.view.Screen;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DelimiterFactory delimiterFactory = new DelimiterFactory();
+        Splitter splitter = new Splitter(delimiterFactory);
+        Calculator calculator = new Adder(splitter);
+
+        Keyboard keyboard = new Keyboard();
+        Screen screen = new Screen();
+
+        CalculateController controller = new CalculateController(calculator, keyboard, screen);
+        controller.run();
     }
 }
--- src/main/java/calculator/application/CalculateController.java
+++ src/main/java/calculator/application/CalculateController.java
@@ -1,5 +1,24 @@
 package calculator.application;
 
-public class CalculateController {
+import calculator.domain.Calculator;
+import calculator.view.Keyboard;
+import calculator.view.Screen;
 
+public class CalculateController {
+    private final Calculator calculator;
+    private final Keyboard keyboard;
+    private final Screen screen;
+
+    public CalculateController(Calculator calculator, Keyboard keyboard, Screen screen) {
+        this.calculator = calculator;
+        this.keyboard = keyboard;
+        this.screen = screen;
+    }
+
+    public void run() {
+        screen.display("덧셈할 문자열을 입력해 주세요.");
+        String expression = keyboard.read();
+        int result = calculator.calculate(expression);
+        screen.display("결과 : " + result);
+    }
 }
